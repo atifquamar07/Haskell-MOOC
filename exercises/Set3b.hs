4c54fd78cc6c4fa86826de90026dbd0ba9f1f198
atifquamar07 <atif20523@iiitd.ac.in>
2024-02-20 14:14:35 +0530

    set 3 done

--- exercises/Set3b.hs
+++ exercises/Set3b.hs
@@ -1,26 +1,3 @@
--- Exercise set 3b
---
--- This is a special exercise set. The exercises are about
--- implementing list functions using recursion and pattern matching,
--- without using any standard library functions. For this reason,
--- you'll be working in a limited environment where almost none of the
--- standard library is available.
---
--- At least the following standard library functions are missing:
---  * (++)
---  * head
---  * tail
---  * map
---  * filter
---  * concat
---  * (!!)
---
--- The (:) operator is available, as is list literal syntax [a,b,c].
---
--- Feel free to use if-then-else, guards, and ordering functions (< and > etc.).
---
--- The tests will check that you haven't added imports :)
-
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Set3b where
@@ -31,157 +8,108 @@ import Mooc.Todo
 ------------------------------------------------------------------------------
 -- Ex 1: given numbers start, count and end, build a list that starts
 -- with count copies of start and ends with end.
---
 -- Use recursion and the : operator to build the list.
---
--- Examples:
---   buildList 1 5 2 ==> [1,1,1,1,1,2]
---   buildList 7 0 3 ==> [3]
-
 buildList :: Int -> Int -> Int -> [Int]
-buildList start count end = todo
+buildList start count end = if count > 0 then start : buildList start (count - 1) end else [end]
 
 ------------------------------------------------------------------------------
 -- Ex 2: given i, build the list of sums [1, 1+2, 1+2+3, .., 1+2+..+i]
---
 -- Use recursion and the : operator to build the list.
---
--- Ps. you'll probably need a recursive helper function
-
 sums :: Int -> [Int]
-sums i = todo
+sums i = go 1
+  where
+    go n
+      | n > i = []
+      | otherwise = customSum 1 n : go (n + 1)
+
+    -- Custom sum function to replace the standard library `sum`
+    customSum start end
+      | start > end = 0
+      | otherwise = start + customSum (start + 1) end
 
 ------------------------------------------------------------------------------
 -- Ex 3: define a function mylast that returns the last value of the
 -- given list. For an empty list, a provided default value is
 -- returned.
---
 -- Use only pattern matching and recursion (and the list constructors : and [])
---
--- Examples:
---   mylast 0 [] ==> 0
---   mylast 0 [1,2,3] ==> 3
-
 mylast :: a -> [a] -> a
-mylast def xs = todo
+mylast def [] = def
+mylast _ (x:[]) = x
+mylast def (_:xs) = mylast def xs
 
 ------------------------------------------------------------------------------
 -- Ex 4: safe list indexing. Define a function indexDefault so that
 --   indexDefault xs i def
 -- gets the element at index i in the list xs. If i is not a valid
 -- index, def is returned.
---
 -- Use only pattern matching and recursion (and the list constructors : and [])
---
--- Examples:
---   indexDefault [True] 1 False         ==>  False
---   indexDefault [10,20,30] 0 7         ==>  10
---   indexDefault [10,20,30] 2 7         ==>  30
---   indexDefault [10,20,30] 3 7         ==>  7
---   indexDefault ["a","b","c"] (-1) "d" ==> "d"
-
 indexDefault :: [a] -> Int -> a -> a
-indexDefault xs i def = todo
+indexDefault [] _ def = def
+indexDefault (x:xs) i def
+  | i == 0 = x
+  | otherwise = indexDefault xs (i - 1) def
 
 ------------------------------------------------------------------------------
 -- Ex 5: define a function that checks if the given list is in
 -- increasing order.
---
 -- Use pattern matching and recursion to iterate through the list.
---
--- Examples:
---   sorted [1,2,3] ==> True
---   sorted []      ==> True
---   sorted [2,7,7] ==> True
---   sorted [1,3,2] ==> False
---   sorted [7,2,7] ==> False
-
 sorted :: [Int] -> Bool
-sorted xs = todo
+sorted [] = True
+sorted [_] = True
+sorted (x:y:xs) = x <= y && sorted (y:xs)
 
 ------------------------------------------------------------------------------
 -- Ex 6: compute the partial sums of the given list like this:
---
 --   sumsOf [a,b,c]  ==>  [a,a+b,a+b+c]
 --   sumsOf [a,b]    ==>  [a,a+b]
 --   sumsOf []       ==>  []
---
 -- Use pattern matching and recursion (and the list constructors : and [])
-
 sumsOf :: [Int] -> [Int]
-sumsOf xs = todo
+sumsOf = go 0
+  where
+    go _ [] = []
+    go acc (x:xs) = let sum = acc + x in sum : go sum xs
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement the function merge that merges two sorted lists of
 -- Ints into a sorted list
---
 -- Use only pattern matching and recursion (and the list constructors : and [])
---
--- Examples:
---   merge [1,3,5] [2,4,6] ==> [1,2,3,4,5,6]
---   merge [1,1,6] [1,2]   ==> [1,1,1,2,6]
-
 merge :: [Int] -> [Int] -> [Int]
-merge xs ys = todo
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+  | x <= y = x : merge xs (y:ys)
+  | otherwise = y : merge (x:xs) ys
 
 ------------------------------------------------------------------------------
 -- Ex 8: compute the biggest element, using a comparison function
 -- passed as an argument.
---
 -- That is, implement the function mymaximum that takes
---
--- * a function `bigger` :: a -> a -> Bool
--- * a value `initial` of type a
--- * a list `xs` of values of type a
---
--- and returns the biggest value it sees, considering both `initial`
--- and all element in `xs`.
---
--- Examples:
---   mymaximum (>) 3 [] ==> 3
---   mymaximum (>) 0 [1,3,2] ==> 3
---   mymaximum (>) 4 [1,3,2] ==> 4    -- initial value was biggest
---   mymaximum (<) 4 [1,3,2] ==> 1    -- note changed biggerThan
---   mymaximum (\(a,b) (c,d) -> b > d) ("",0) [("Banana",7),("Mouse",8)]
---     ==> ("Mouse",8)
-
 mymaximum :: (a -> a -> Bool) -> a -> [a] -> a
-mymaximum bigger initial xs = todo
+mymaximum _ initial [] = initial
+mymaximum bigger initial (x:xs)
+  | bigger x initial = mymaximum bigger x xs
+  | otherwise = mymaximum bigger initial xs
 
 ------------------------------------------------------------------------------
 -- Ex 9: define a version of map that takes a two-argument function
 -- and two lists. Example:
---
 --   map2 f [x,y,z,w] [a,b,c]  ==> [f x a, f y b, f z c]
---
 -- If the lists have differing lengths, ignore the trailing elements
 -- of the longer list.
---
 -- Use recursion and pattern matching. Do not use any library functions.
-
 map2 :: (a -> b -> c) -> [a] -> [b] -> [c]
-map2 f as bs = todo
+map2 _ [] _ = []
+map2 _ _ [] = []
+map2 f (a:as) (b:bs) = f a b : map2 f as bs
 
 ------------------------------------------------------------------------------
 -- Ex 10: implement the function maybeMap, which works a bit like a
 -- combined map & filter.
----
 -- maybeMap is given a list ([a]) and a function of type a -> Maybe b.
--- This function is called for all values in the list. If the function
--- returns Just x, x will be in the result list. If the function
--- returns Nothing, no value gets added to the result list.
---
--- Examples:
---
--- let f x = if x>0 then Just (2*x) else Nothing
--- in maybeMap f [0,1,-1,4,-2,2]
---   ==> [2,8,4]
---
--- maybeMap Just [1,2,3]
---   ==> [1,2,3]
---
--- maybeMap (\x -> Nothing) [1,2,3]
---   ==> []
-
 maybeMap :: (a -> Maybe b) -> [a] -> [b]
-maybeMap f xs = todo
+maybeMap _ [] = []
+maybeMap f (x:xs) =
+  case f x of
+    Just y -> y : maybeMap f xs
+    Nothing -> maybeMap f xs
